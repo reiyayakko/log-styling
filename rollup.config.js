c593cf146979393e6a84d4c34afdfb6a8df14ca3
reiyayakko <reiyayakko@gmail.com>
2021-06-20 14:24:36 +0900

    chore: add setting files

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+
+// @ts-check
+
+import ts from "@wessberg/rollup-plugin-ts";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
+
+const name = "LogStyling";
+const banner = `
+/**
+ * @license
+ * ${pkg.name} v${pkg.version}
+ * Copyright 2021 reiyayakko
+ * License MIT
+ */`.slice(1);
+
+/** @type {import("rollup").RollupOptions} */
+export default ({
+    input: "./src/index.ts",
+    output: [
+        {
+            file: `dist/${pkg.name}.es.js`,
+            format: "es",
+            banner,
+        },
+        {
+            file: `dist/${pkg.name}.umd.js`,
+            format: "umd",
+            name,
+            banner,
+        },
+        {
+            file: `dist/${pkg.name}.umd.min.js`,
+            format: "umd",
+            name,
+            banner,
+            plugins: [
+                terser({
+                    mangle: { properties: { regex: /^_(?!_)/ } }
+                }),
+            ],
+        },
+    ],
+    plugins: [ts()],
+});
