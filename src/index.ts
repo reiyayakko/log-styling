c8c8feeab9dcfbf3aa4349e7ebe595928c0f301f
reiyayakko <reiyayakko@gmail.com>
2021-06-20 16:33:06 +0900

    feat: extract static methods

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,82 @@
 
-type LogArgs = [string, ...unknown[]];
+type LogElementOrString = string | LogElement;
+type LogNode = LogElementOrString | (LogElementOrString | LogElementOrString[])[];
 
-type LogStylingOrString = string | LogStyling;
-type Loggable = LogStylingOrString | (LogStylingOrString | LogStylingOrString[])[];
+const concat = <T>(arr: T[], carr: T | ConcatArray<T>): T[] => arr.concat(carr);
 
-const concat = <T>(accum: T[], val: T | ConcatArray<T>): T[] => accum.concat(val);
-
-export class LogStyling {
-    protected constructor(protected msg: string, protected sub: unknown) {}
-    private static _fit(stylingable: LogStylingOrString): LogStyling {
-        return stylingable instanceof LogStyling
-            ? stylingable
-            : LogStyling.string(stylingable)
+export class LogElement {
+    constructor(readonly msg: string, readonly sub: unknown) {}
+    private static _fit(logNode: LogElementOrString): LogElement {
+        return logNode instanceof LogElement
+            ? logNode
+            : string(logNode)
     }
-    protected static fitLogStylingArray(loggable: Loggable): LogStyling[] {
-        return Array.isArray(loggable)
-            ? loggable.reduce<LogStylingOrString[]>(concat, []).map(LogStyling._fit)
-            : [LogStyling._fit(loggable)];
+    static fitLogElements(logNode: LogNode): LogElement[] {
+        return Array.isArray(logNode)
+            ? logNode.reduce<LogElementOrString[]>(concat, []).map(LogElement._fit)
+            : [LogElement._fit(logNode)];
     }
-    /**
-     * Outputs a string.
-     */
-    static string(string: string) {
-        return new LogStyling("%s", string);
-    }
-    /**
-     * Outputs a floating point number.
-     */
-    static float(float: number, digit = 0) {
-        return new LogStyling(`%.${digit | 0}f`, float);
-    }
-    /**
-     * Outputs an integer.
-     */
-    static int(int: number, digit = 0) {
-        return new LogStyling(`%.${digit | 0}i`, int);
-    }
-    /**
-     * Output the log using `optimally useful formatting`.
-     */
-    static object(object: unknown) {
-        return new LogStyling("%o", object);
-    }
-    /**
-     * Output the log using `generic JavaScript object formatting`.
-     */
-    static generic(object: unknown) {
-        return new LogStyling("%O", object);
-    }
-    /**
-     * Applies the style to the letters output to the console.
-     * Styles previously applied in `LogStyling.style` will be invalid.
-     * @param css The style to be applied.
-     * @see https://developer.mozilla.org/docs/Web/API/console#styling_console_output
-     */
-    static style(css: string) {
-        return new LogStyling("%c", css);
-    }
-    /**
-     * Resets the style specified in {@link LogStyling.style}.
-     */
-    static readonly resetStyle = LogStyling.style("");
-    /**
-     * Apply the style to the inner log.
-     */
-    static styled(css: string, loggable: Loggable): LogStyling[] {
+    static toLog(logNode: LogNode): LogArgs {
+        const elements = LogElement.fitLogElements(logNode);
         return [
-            LogStyling.style(css),
-            ...LogStyling.fitLogStylingArray(loggable).map(styling =>
-                styling.msg === "%c" ? LogStyling.style(css + ";" + styling.sub) : styling
-            ),
-            LogStyling.resetStyle,
+            elements.reduce((accum, logEl) => accum + logEl.msg, ""),
+            ...elements.map(logEl => logEl.sub)
         ];
-    }
-    static toLog(loggable: Loggable): LogArgs {
-        const stylings = LogStyling.fitLogStylingArray(loggable);
-        return [
-            stylings.reduce((accum, styling) => accum + styling.msg, ""),
-            ...stylings.map(styling => styling.sub)
-        ];
-    }
-    static log(logfn: (...subs: LogArgs) => void, loggable: Loggable): void {
-        logfn(...LogStyling.toLog(loggable));
     }
 }
+
+/**
+ * Outputs a string.
+ */
+export const string = (string: string) => new LogElement("%s", string);
+
+/**
+ * Outputs a floating point number.
+ */
+export const float = (float: number, digit = 0) => new LogElement(`%.${digit | 0}f`, float);
+
+/**
+ * Outputs an integer.
+ */
+export const int = (int: number, digit = 0) => new LogElement(`%.${digit | 0}i`, int);
+
+/**
+ * Output the log using `optimally useful formatting`.
+ */
+export const object = (object: unknown) => new LogElement("%o", object);
+
+/**
+ * Output the log using `generic JavaScript object formatting`.
+ */
+export const generic = (object: unknown) => new LogElement("%O", object);
+
+/**
+ * Applies the style to the letters output to the console.
+ * Styles previously applied in `style` will be invalid.
+ * @see https://developer.mozilla.org/docs/Web/API/console#styling_console_output
+ */
+export const style = (css: string) => new LogElement("%c", css);
+
+/**
+ * Resets the style specified in {@link style}.
+ */
+export const resetStyle = style("");
+
+/**
+ * Apply the style to the inner log.
+ */
+export const styled = (css: string, logNode: LogNode): LogElement[] => {
+    return [
+        style(css),
+        ...LogElement.fitLogElements(logNode).map(logEl =>
+            logEl.msg === "%c" ? style(css + ";" + logEl.sub) : logEl
+        ),
+        resetStyle,
+    ];
+};
+
+type LogArgs = [string, ...unknown[]];
+
+export const log = (logfn: (...args: LogArgs) => void, logNode: LogNode): void => {
+    logfn(...LogElement.toLog(logNode));
+};
